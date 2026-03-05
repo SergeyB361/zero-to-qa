d7be30db8ccc670a9e94140ff70617c1b4d4b357
Sergey Bezruchenko <s.bezruchenko@gmail.com>
2026-03-05 04:53:22 +0300

    feat(week1-day7): complete calculator mini-project and finish week 1

--- week_01/day_07_miniproject_calculator/calculator.py
+++ week_01/day_07_miniproject_calculator/calculator.py
@@ -2,48 +2,73 @@
 
 
 def add(a, b):
-    pass
+    return round((a+b), 2)
 
 
 def sub(a, b):
-    pass
+    return round((a-b), 2)
 
 
 def mul(a, b):
-    pass
+    return round((a*b), 2)
 
 
 def div(a, b):
-    pass
+    return round((a/b), 2) if b !=0 else "Division by zero"
 
 
 def read_number(prompt):
-    return print("Число: ") 
-
-
+    while True:
+        try:
+            input_number = float(input(f"{prompt} = "))
+            return input_number
+        except ValueError:
+            print("Это не число!")
+    
 def main():
     print("Консольный калькулятор, выбери пункт меню и введи символ.")
 
     menu = {
-        "+":"сложение",
-        "-":"вычитание",
-        "*":"умножение",
-        "/":"деление",
-        "q":"выход"
+        "+": "сложение",
+        "-": "вычитание",
+        "*": "умножение",
+        "/": "деление",
+        "q": "выход"
     }
  
     while True:
-        for key in menu.items():
-            print(f"{key}")
+        a = b = 0.0
+        for key, value in menu.items():
+            print(f"{key} {value}")
 
         menu_choice = input("Ваш выбор: ")
 
         if menu_choice == "q":
             print("Выход")
             break
+
         elif menu_choice == "+":
-            print("Сложение чисел:")
-            add(a, b)
+            a = read_number("Введите первое число:")
+            b = read_number("Введите второе число")
+            print(f"Результат: {add(a, b)}")
+
+        elif menu_choice == "-":
+            a = read_number("Введите первое число:")
+            b = read_number("Введите второе число")
+            print(f"Результат: {sub(a, b)}")
+
+        elif menu_choice == "*":
+            a = read_number("Введите первое число:")
+            b = read_number("Введите второе число")
+            print(f"Результат: {mul(a, b)}")
+
+        elif menu_choice == "/":
+            a = read_number("Введите первое число:")
+            b = read_number("Введите второе число")
+            print(f"Результат: {div(a, b)}")    
+        else: 
+            print("Unknown operation")      
+
 
 
 if __name__ == "__main__":
