c83ac1c895e65f2e2cbcb6da156660c12b1eee1b
Sergey Bezruchenko <s.bezruchenko@gmail.com>
2026-03-06 06:36:37 +0300

    feat(week2-day1): complete lists practice and update progress

--- week_02/day_01_lists/practice.py
+++ week_02/day_01_lists/practice.py
@@ -11,6 +11,29 @@ statuses = [200, 404, 500, 200, 301, 404]
 # 4) Выведи итоговый список
 # Напиши код здесь:
 
+# 1)
+print(statuses[0])
+print(statuses[-1])
+
+# 2) # Вариант A: через list comprehension
+st_404 = 0
+st_404 = len([status for status in statuses if status == 404])
+print(st_404)
+
+# 2) # Вариант B: через цикл for
+st_404 = 0
+for status in statuses:
+    if status == 404:
+        st_404 = st_404 + 1
+print(st_404)
+
+# 3)
+statuses.append(201)
+
+# 4)
+print(statuses)
+
+
 
 # Задание 2
 # Дан список nums = [7, 2, 9, 2, 1]
@@ -21,6 +44,15 @@ statuses = [200, 404, 500, 200, 301, 404]
 # 3) Выведи оба списка
 # Напиши код здесь:
 
+nums = [7, 2, 9, 2, 1]
+nums_sort = sorted(nums)
+nums.sort(reverse=True)
+
+print(nums)
+print(nums_sort)
+
+
+
 
 # Задание 3
 # Используя list comprehension, создай список квадратов чисел от 1 до 10,
@@ -29,6 +61,11 @@ statuses = [200, 404, 500, 200, 301, 404]
 # Ожидается: [4, 16, 36, 64, 100]
 # Напиши код здесь:
 
+square_list = []
+square_list = list(x*x for x in range(1,11) if x % 2 == 0)
+# square_list = [x * x for x in range(1, 11) if x % 2 == 0] вариант без list()
+print(square_list)
+
 
 # Задание 4
 # Дан список users = ["ann", "bob", "kate"]
@@ -36,3 +73,9 @@ statuses = [200, 404, 500, 200, 301, 404]
 # С помощью list comprehension создай новый список
 # в формате: ["user: ann", "user: bob", "user: kate"]
 # Напиши код здесь:
+
+users = ["ann", "bob", "kate"]
+
+#new_users = [] - можно убрать
+new_users = [f"user: {x}" for x in users]
+print(new_users)
