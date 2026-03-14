f6e7e9cbe016b1ab8139ce7b7380b6f7be5ecdfa
Sergey Bezruchenko <s.bezruchenko@gmail.com>
2026-03-14 06:01:27 +0300

    feat(week2): save day6 progress and add day7 json parser project

---
+++ week_02/day_07_miniproject_json_parser/json_parser.py
@@ -0,0 +1,30 @@
+import json
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent
+USERS_FILE = BASE_DIR / "users.json"
+
+
+def load_users(path):
+    pass
+
+
+def get_names(users):
+    pass
+
+
+def get_active_users(users):
+    pass
+
+
+def get_average_age(users):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
