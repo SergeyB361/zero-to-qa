1be18cd1843e03b09deb4f476536e78c343a2763
Sergey Bezruchenko <s.bezruchenko@gmail.com>
2026-03-18 15:01:37 +0300

    feat(week3-day4): complete regex practice and update progress

--- week_03/day_04_regex/practice.py
+++ week_03/day_04_regex/practice.py
@@ -6,10 +6,18 @@ import re
 # Дан текст: "User id: 12345"
 # Найди первое число через re.search и выведи его.
 
+text = "User id: 12345"
+number_first = re.search(r'\d+', text)
+print(int(number_first.group()))
+
 
 # Задание 2
 # Дан текст: "Codes: 200 404 500"
 # Найди все числа через re.findall и выведи список.
+
+text = "Codes: 200 404 500"
+number_all = re.findall(r'\d+', text)
+print(number_all)
 
 
 # Задание 3
@@ -17,8 +25,17 @@ import re
 # Проверь через regex, что строка похожа на email.
 # Выведи True или False.
 
+email = "qa.team@example.com"
+is_email = re.match(r'[\w.]+@[\w.]+\.\w+', email)
+#print(is_email.group())
+print(bool(is_email))
 
 # Задание 4
 # Дан текст: "Order 123 created at 10:45"
 # Замени все цифры на символ "X" через re.sub.
 # Выведи результат.
+
+text = "Order 123 created at 10:45"
+text_change = re.sub(r'\d+', "X", text)
+
+print(text_change)
